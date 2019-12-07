c06b0001286d82628b11fb55001ef714cb7c44f7
Phil <phil@easya.io>
2019-12-07 21:34:49 +0000

    naming

--- services/receive.js
+++ services/receive.js
@@ -16,7 +16,7 @@ const Response = require("./response"),
   // Care = require("./care"),
   // Survey = require("./survey"),
   GraphAPi = require("./graph-api"),
-  // fs = require("fs"),
+  fs = require("fs"),
   request = require("request"),
   i18n = require("../i18n.config");
 
@@ -137,6 +137,19 @@ module.exports = class Receive {
     console.log("Received attachment:", `${attachment} for ${this.user.psid}`);
 
     // console.log(fs.readFileSync(attachment).buffer.toString("base64"));
+    // console.log(Buffer.from(attachment).toString("base64"));
+
+    // function to encode file data to base64 encoded string
+    function base64_encode(file) {
+      // read binary data
+      const bitmap = fs.readFileSync(file);
+      // convert binary data to base64 encoded string
+      return new Buffer(bitmap).toString("base64");
+    }
+
+    // convert image to base64 encoded string
+    const base64str = base64_encode(attachment);
+    console.log(base64str);
 
     response = Response.genText(
       "Thanks for sending this picture!!! I'm just analysing it now."
@@ -152,7 +165,7 @@ module.exports = class Receive {
       "http://127.0.0.1:5000/predict",
       {
         json: {
-          base64: "base64 image"
+          imgString: "base64 image"
         }
       },
       (error, res, body) => {
