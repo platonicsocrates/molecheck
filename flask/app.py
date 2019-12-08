b72596b369640a8a26dedc085da357608b90164c
amarchin <andreamarchini87@gmail.com>
2019-12-08 15:50:09 +0100

    Add ML API to Flask

--- flask/app.py
+++ flask/app.py
@@ -1,21 +1,20 @@
 import io
 import json
-import requests
-import matplotlib.pyplot as plt
 
-from torchvision import models
+import requests
+import torch
 import torchvision.transforms as transforms
 from PIL import Image
 from flask import Flask, jsonify, request
-import base64
 
+from class_to_celeb import class_to_celeb
+from model_loading import load_model
 
 app = Flask(__name__)
 
 # TODO Update with our skin disease model
-#imagenet_class_index = json.load(open('<PATH/TO/.json/FILE>/imagenet_class_index.json'))
-#model = models.densenet121(pretrained=True)
-#model.eval()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = load_model('model_face.pth')
 
 
 def transform_image(image_bytes):
@@ -28,48 +27,38 @@ def transform_image(image_bytes):
     :return: transformed image
     """
 
-    my_transforms = transforms.Compose([transforms.Resize(255),
-                                        transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
+    transformations = transforms.Compose([
+        transforms.Resize([224, 224]),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.49021736, 0.4212893, 0.39216894], std=[0.49021736, 0.4212893, 0.39216894])
+    ])
+
     image = Image.open(io.BytesIO(image_bytes))
-    return my_transforms(image).unsqueeze(0)
+    return transformations(image).unsqueeze(0)
 
 
-def get_prediction(image_bytes):
+def get_prediction(image_bytes) -> str:
     """
     Predict class for image
     :param image_bytes: input image
     :return: prediction
     """
     tensor = transform_image(image_bytes=image_bytes)
+    tensor = tensor.to(device)
     outputs = model.forward(tensor)
     _, y_hat = outputs.max(1)
-    predicted_idx = str(y_hat.item())
-    return imagenet_class_index[predicted_idx]
+    predicted_idx = y_hat.item()
+    return class_to_celeb(predicted_idx)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-
         img_dict = json.loads(json.dumps(request.json))
-        print(img_dict)
         r = requests.get(img_dict["imgUrl"])
-        print(r.content)
-        with open('mark.png', 'wb') as f:
-            f.write(r.content)
-
-
-        #file = request.files['file']
-        #img_bytes = file.read()
-        #imgdata = base64.b64decode(imgstring)
-        #print("received")
-        #class_id, class_name = get_prediction(image_bytes=img_bytes)
-        #return jsonify({'class_id': class_id, 'class_name': class_name})
-        return jsonify({'lookalike': 'a mark'})
+        img_bytes = r.content
+        name = get_prediction(img_bytes)
+        return jsonify({'lookalike': name})
 
 
 if __name__ == '__main__':
---
+++ flask/model_loading.py
@@ -0,0 +1,20 @@
+import torch
+from torchvision import models
+
+
+def get_resnext() -> models.resnext101_32x8d:
+    resnext = models.resnext101_32x8d(pretrained=True)
+    for param in resnext.parameters():
+        param.requires_grad = False
+    num_ftrs = resnext.fc.in_features
+    resnext.fc = torch.nn.Linear(num_ftrs, 190)
+    if torch.cuda.is_available():
+        resnext.cuda()
+    return resnext
+
+
+def load_model(model_path: str) -> models:
+    model = get_resnext()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    return model
